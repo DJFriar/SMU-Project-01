d7a590fb832bf2c2cd7cb8e22e9c0053f0a72ebc
Jose Sanchez-Capo <josesanchezcapo@me.com>
2020-11-10 09:42:52 -0600

    adding Fema Call

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,22 @@
-// Create a call base on city input
+var state = 'TX'; // testing
+
+
+//var myCities = JSON.parse(uscities);
+
 
 // Get County based on city input using local json file
 
-// make pprimary call to fema api
+// Call to FEMA API
+var queryURL = "https://www.fema.gov/api/open/v2/DisasterDeclarationsSummaries?$filter=state%20eq%20%27"+state+"%27";
 
-// render values for json file to main page
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    }).then(function (response) {
+        console.log(response);
+        console.log(queryURL);
+
+          console.log(response.declarationTitle);
+    });
+
+
