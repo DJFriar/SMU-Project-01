c98cdc8e8f04573c95315a9b55e1938fe2d292c6
Jose Sanchez-Capo <josesanchezcapo@me.com>
2020-11-10 12:56:52 -0600

    working on logic for data rendering

--- assets/script.js
+++ assets/script.js
@@ -1,49 +1,54 @@
-// var state = 'TX'; // testing
+var state = '';
 var city = 'Little Elm';
+var county = '';
 
 // County Call local json file
 
 $.getJSON("https://raw.githubusercontent.com/DJFriar/SMU-Project-01/Jose/assets/json/uscities.json", function (data) {
 
-    // Sample data for field mapping
-    //  console.log('county name: '+ data[0].county_name);
-    //  console.log('city name: ' + data[0].city);
-    //  console.log('state: ' + data[0].state_id);
-    //  console.log('state name: ' + data[0].state_name)
+    // **** DON'T DELETE - Sample data for field mapping  
+    //      console.log('county name: '+ data[0].county_name);
+    //      console.log('city name: ' + data[0].city);
+    //      console.log('state: ' + data[0].state_id);
+    //      console.log('state name: ' + data[0].state_name)
 
-    for(var i  in data) {
-        if(data[i].city === city) {
-            console.log('county name: '+ data[i].county_name);
-            console.log('city name: ' + data[i].city);
-            console.log('state: ' + data[i].state_id);
-            console.log('state name: ' + data[i].state_name)
+    for (var i in data) {
+        if (data[i].city === city) {
+            // console.log('county name: ' + data[i].county_name);
+            // console.log('city name: ' + data[i].city);
+            // console.log('state: ' + data[i].state_id);
+            // console.log('state name: ' + data[i].state_name)
+            state = data[i].state_id;
+            county = data[i].county_name;
         }
     }
-    
-})
+
+    // Call to FEMA API
+    var queryURL = "https://www.fema.gov/api/open/v2/DisasterDeclarationsSummaries?$filter=state%20eq%20%27" + state + "%27";
+
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    }).then(function (response) {
+        console.log(response);
+        console.log(queryURL);
+
+        for (var i in response) {
+
+            // Sample data for field mapping
+            console.log(response.metadata.filter);
+            console.log(response.DisasterDeclarationsSummaries[i].declarationDate)
+            console.log(response.DisasterDeclarationsSummaries[i].incidentBeginDate)
+            console.log(response.DisasterDeclarationsSummaries[i].incidentEndDate)
+            console.log(response.DisasterDeclarationsSummaries[i].incidentType)
+            console.log(response.DisasterDeclarationsSummaries[i].declarationTitle)
+            console.log(response.DisasterDeclarationsSummaries[i].fipsStateCode)
+            console.log(response.DisasterDeclarationsSummaries[i].fipsCountyCode)
+            console.log(response.DisasterDeclarationsSummaries[i].designatedArea)
+
+        }
 
 
+    });
 
-// Call to FEMA API
-var queryURL = "https://www.fema.gov/api/open/v2/DisasterDeclarationsSummaries?$filter=state%20eq%20%27" + state + "%27";
-
-$.ajax({
-    url: queryURL,
-    method: 'GET'
-}).then(function (response) {
-    console.log(response);
-    console.log(queryURL);
-
-    // Sample data for field mapping
-    console.log(response.metadata.filter);
-    console.log(response.DisasterDeclarationsSummaries[0].declarationDate)
-    console.log(response.DisasterDeclarationsSummaries[0].incidentBeginDate)
-    console.log(response.DisasterDeclarationsSummaries[0].incidentEndDate)
-    console.log(response.DisasterDeclarationsSummaries[0].incidentType)
-    console.log(response.DisasterDeclarationsSummaries[0].declarationTitle)
-    console.log(response.DisasterDeclarationsSummaries[0].fipsStateCode)
-    console.log(response.DisasterDeclarationsSummaries[0].fipsCountyCode)
-    console.log(response.DisasterDeclarationsSummaries[0].designatedArea)
 });
-
-
