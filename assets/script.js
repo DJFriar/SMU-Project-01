88cdc2be03ff3bcf688f70f73bb0cc4970d4fa14
Jose Sanchez-Capo <josesanchezcapo@me.com>
2020-11-13 17:12:45 -0600

    last code

--- assets/script.js
+++ assets/script.js
@@ -1,54 +1,114 @@
-var cityLat = '32.77';
-var cityLong = '-96.78';
+var inputtedCity = "";
+var lat = "";
+var long = "";
+var cityIndex = 0;
 
-  // Calling census api , with lay long get county
-    var queryCityInfo =  "https://geo.fcc.gov/api/census/area?lat=" + cityLat + "&lon=" + cityLong + "&format=json";
 
-    $.ajax({
-        url: queryCityInfo,
-        method: 'GET'
-    }).then(function (queryCityInfo) {
-        console.log(queryCityInfo.results[0].county_name);
-        console.log(queryCityInfo.results[0].state_code);
-        console.log(queryCityInfo.results[0].state_name);
+// Handle Search Button
+$("#searchBtn").on("click", function (event) {
+    event.preventDefault();
+    // capture the user's input
+    var rawInputtedCity = $("#searchField").val();
+    // make everything title case and pretty
+    inputtedCity = rawInputtedCity.toLowerCase().replace(/\b[a-z]/g, function (txtVal) {
+        return txtVal.toUpperCase();
+    });
+    // add city to the search history
+    logCityToHistoryArea();
+    // fetch the weather data
+    buildCity();
+});
 
-        var state = queryCityInfo.results[0].state_code;
-        var county = queryCityInfo.results[0].county_name;
+function logCityToHistoryArea() {
+    var historyListItem = $("<li>").attr("class", "list-group-item");
+    historyListItem.text(inputtedCity);
+    $("#cityList").append(historyListItem);
+};
+
+function buildCity() {
+    // create the section for each city
+    var cityDiv = $("<div>").attr("class", "uk-child-width-expand@s uk-text-center");
+    var dataGrid = $("<div uk-grid>").attr("class", "uk-child-width-expand@s uk-text-center");
+    cityDiv.text(inputtedCity);
+    cityDiv.append(dataGrid);
+    // add the new city section to the main content area
+    $("#mainContent").append(cityDiv);
+    // add the first data card to the city row
+    buildWeatherCard(dataGrid);
+};
+
+function buildWeatherCard(dataGrid) {
+
+    // create an empty div and add it to the city row
+    var emptyDiv = $("<div>").attr("class", "dataCard");
+    // create a card to hold the data returned by the API
+    var cardDiv = $("<div>").attr("class", "uk-card uk-card-body");
+    // set the card title
+    var cardTitle = $("<h3>").attr("class", "uk-card-title");
+    // add the new data card to the city row
+    cardTitle.text("Weather");
+    cardDiv.append(cardTitle);
+    emptyDiv.append(cardDiv);
+    dataGrid.append(emptyDiv);
+    // call next function
+    buildFEMACard(dataGrid);
+};
+
+
+function buildFEMACard(dataGrid) {
+
+    var queryUrl = "https://geo.fcc.gov/api/census/area?lat=" + lat + "&lon=" + long + "&format=json";
+    $.ajax({ url: queryUrl, method: 'GET' }).then(function (response) {
+        var state = response.results[0].state_code; var county = response.results[0].county_name
+        var queryUrl = "https://geo.fcc.gov/api/census/area?lat=" + lat + "&lon=" + long + "&format=json";
 
         // Call to FEMA API
         var queryFemaInfo = "https://www.fema.gov/api/open/v2/DisasterDeclarationsSummaries?$filter=state%20eq%20%27" + state + "%27";
-
-        $.ajax({
-            url: queryFemaInfo,
-            method: 'GET'
-        }).then(function (femaInfo) {
-            console.log('fema: ' + queryFemaInfo);
-
+        $.ajax({ url: queryFemaInfo, method: 'GET' }).then(function (femaInfo) {
             for (var b in femaInfo.DisasterDeclarationsSummaries) {
-
-
                 if (femaInfo.DisasterDeclarationsSummaries[b].state === state && femaInfo.DisasterDeclarationsSummaries[b].designatedArea.match(county)
-                && femaInfo.DisasterDeclarationsSummaries[b].fyDeclared === 1990) {
-
-                  console.log(femaInfo.DisasterDeclarationsSummaries[b].state)
-                    console.log('Designated Area:' + femaInfo.DisasterDeclarationsSummaries[b].designatedArea)
-                    console.log('Incident Type:' + femaInfo.DisasterDeclarationsSummaries[b].incidentType)
-                    console.log(femaInfo.DisasterDeclarationsSummaries[b].declarationTitle)
-                    console.log(femaInfo.DisasterDeclarationsSummaries[b].declarationDate)
-                    console.log(femaInfo.DisasterDeclarationsSummaries[b].incidentBeginDate)
-                    console.log('Year: ' + femaInfo.DisasterDeclarationsSummaries[b].fyDeclared)
-
-                    $('.1').append('Year: ' + femaInfo.DisasterDeclarationsSummaries[b].fyDeclared);
-                    $('.2').append('Incident Type: ' + femaInfo.DisasterDeclarationsSummaries[b].incidentType);
-                    $('.3').append('Designated Area: ' + femaInfo.DisasterDeclarationsSummaries[b].designatedArea);
-
+                    && femaInfo.DisasterDeclarationsSummaries[b].fyDeclared > 1990) {
+                    var designatedArea = (femaInfo.DisasterDeclarationsSummaries[b].designatedArea);
+                    var type = (femaInfo.DisasterDeclarationsSummaries[b].incidentType);
+                    var title = (femaInfo.DisasterDeclarationsSummaries[b].declarationTitle)
+                    var year = (femaInfo.DisasterDeclarationsSummaries[b].fyDeclared)
+                }
             }
+            // create an empty div and add it to the city row
+            var emptyDiv = $("<div>").attr("class", "dataCard");
+            // create a card to hold the data returned by the API
+            var cardDiv = $("<div>").attr("class", "uk-card uk-card-body");
+            // set the card title
+            var cardTitle = $("<h3>").attr("class", "uk-card-title");
+            // add the new data card to the city row
+            cardTitle.text("FEMA");
+            cardDiv.append('<ul style="list-style-type:none;text-align: left;"><li>Last disaster declaration: ' + year + '</li><li> Type: ' + type + '</li><li> Area: ' + designatedArea + '</li></ul>');
+            emptyDiv.append(cardDiv);
+            dataGrid.append(emptyDiv);
+            // call next function
 
-        }
+            buildAQICard(dataGrid);
+        });
+    });
+};
 
-    }); 
+function buildAQICard(dataGrid) {
+    // create an empty div
+    var emptyDiv = $("<div>").attr("class", "dataCard");
+    // create a card to hold the data returned by the API
+    var cardDiv = $("<div>").attr("class", "uk-card uk-card-body");
+    // set the card title
+    var cardTitle = $("<h3>").attr("class", "uk-card-title");
+    // add the new data card to the city row
+    cardTitle.text("Air Quality");
+    cardDiv.append(cardTitle);
+    emptyDiv.append(cardDiv);
+    dataGrid.append(emptyDiv);
+};
 
-});
+
+
+
 
 
 
