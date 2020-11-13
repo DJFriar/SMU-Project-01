7f9abed4d1507f24e3910f9223c8c55d4b9f5f21
DJFriar <tommy.craft@icloud.com>
2020-11-12 20:05:05 -0600

    data grid layout working

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,6 @@
 var inputtedCity = "";
+var lat = "";
+var long = "";
 var cityIndex = 0;
 
 // Handle Search Button
@@ -22,22 +24,64 @@ $("#searchBtn").on("click", function(event) {
 function logCityToHistoryArea() {
     var historyListItem = $("<li>").attr("class","list-group-item");
     historyListItem.text(inputtedCity);
-    $("#cityList").prepend(historyListItem);
+    $("#cityList").append(historyListItem);
 };
 
 function buildCity() {
+    // create the section for each city
     var cityDiv = $("<div>").attr("class", "uk-child-width-expand@s uk-text-center");
-    cityDiv.data("cid",cityIndex);
+    var dataGrid = $("<div uk-grid>").attr("class", "uk-child-width-expand@s uk-text-center");
     cityDiv.text(inputtedCity);
+    cityDiv.append(dataGrid);
+    // add the new city section to the main content area
     $("#mainContent").append(cityDiv);
-    cityIndex++;
-    buildCityCard();
+    // add the first data card to the city row
+    buildAQICard(dataGrid);
+
 };
 
-function buildCityCard() {
-    var emptyDiv = $("<div>");
+function buildAQICard(dataGrid) {
+    // create an empty div
+    var emptyDiv = $("<div>").attr("class", "dataCard");
+    // create a card to hold the data returned by the API
     var cardDiv = $("<div>").attr("class", "uk-card uk-card-body");
+    // set the card title
     var cardTitle = $("<h3>").attr("class", "uk-card-title");
-    $(cardDiv).append(emptyDiv);
-    $(emptyDiv).append(cardDiv);
+    // add the new data card to the city row
+    cardTitle.text("Air Quality");
+    cardDiv.append(cardTitle);
+    emptyDiv.append(cardDiv);
+    dataGrid.append(emptyDiv);
+    // call next function
+    buildFEMACard(dataGrid);
+};
+
+function buildFEMACard(dataGrid) {
+    // create an empty div and add it to the city row
+    var emptyDiv = $("<div>").attr("class", "dataCard");
+    // create a card to hold the data returned by the API
+    var cardDiv = $("<div>").attr("class", "uk-card uk-card-body");
+    // set the card title
+    var cardTitle = $("<h3>").attr("class", "uk-card-title");
+    // add the new data card to the city row
+    cardTitle.text("FEMA");
+    cardDiv.append(cardTitle);
+    emptyDiv.append(cardDiv);
+    dataGrid.append(emptyDiv);
+    // call next function
+    buildWeatherCard(dataGrid);
+};
+
+function buildWeatherCard(dataGrid) {
+    // create an empty div and add it to the city row
+    var emptyDiv = $("<div>").attr("class", "dataCard");
+    // create a card to hold the data returned by the API
+    var cardDiv = $("<div>").attr("class", "uk-card uk-card-body");
+    // set the card title
+    var cardTitle = $("<h3>").attr("class", "uk-card-title");
+    // add the new data card to the city row
+    cardTitle.text("Weather");
+    cardDiv.append(cardTitle);
+    emptyDiv.append(cardDiv);
+    dataGrid.append(emptyDiv);
 };
